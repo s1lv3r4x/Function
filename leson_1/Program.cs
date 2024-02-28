9d222ba36fb7b535f16edbccc528b75d4cc461de
Kris <isaevachristin@gmail.com>
2024-02-29 01:57:45 +0300

    добавила папки с упражнениями

---
+++ leson_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+//Программа завершается при вводе символа 
+//‘q’ или при вводе числа, сумма цифр которого чётная.
+int numb;
+int Function (int lost)
+    {
+        int s = 0;
+        while (lost > 0)
+        {
+            s = s + lost % 10;
+            lost = lost / 10;
+        }
+         return s;
+        }
+        
+while (true)
+{
+    Console.Write("Введите любое целое число или q чтобы выйти из программы: ");
+    string? input = Console.ReadLine();
+    bool number = int.TryParse(input, out numb);
+    if (input == "q" | Function(numb) % 2 == 0 )
+    {
+        Console.WriteLine("Выход");
+        break;
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
