f48379ce46cb7a359a50ccc2864cdd10f929220a
Kris <isaevachristin@gmail.com>
2024-02-29 02:03:13 +0300

    добавила упражнение 2 и 3

---
+++ Leson_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] array = new int[10];
+Random rand = new Random();
+void PrintArray(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+
+
+    Console.Write($" четных: {result}");
+
+}
+
+int[] RandomArray()
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 999);
+        Console.Write($"[ {array[i] } ]");
+    }
+    return array;
+}
+PrintArray(RandomArray());
+
---
+++ Leson_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+int []array = new int[10];
+Random rand = new Random();
+
+int [] RandomArray()
+{
+for (int i=0; i< array.Length; i++)
+{
+array[i]=rand.Next(1,10);
+//Console.Write ($"[{array[i]}] ");
+}
+return array;
+}
+
+void PrintArrayReverse(int[]array)
+    {
+        for (int a = array.Length-1; a>=0; a--)
+    {
+        Console.Write($"({array[a]}) ");
+    }
+    }
+PrintArrayReverse(RandomArray());
+
+
